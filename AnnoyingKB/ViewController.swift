73ea4eecd56e06c4a0bb778324f67e40c7311fdd
Lacie <dark830917@gmail.com>
2020-01-04 22:30:52 +0800

    Initial Commit

---
+++ AnnoyingKB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnnoyingKB
+//
+//  Created by 黃士軒 on 2020/1/4.
+//  Copyright © 2020 Lacie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
