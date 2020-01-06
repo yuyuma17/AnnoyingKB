064902cba7da5180f5b3090e5519eee8c1b7f39e
Lacie <dark830917@gmail.com>
2020-01-06 18:23:46 +0800

    完成方法1

--- AnnoyingKB/Controller/PushBySVViewController.swift
+++ AnnoyingKB/Controller/PushBySVViewController.swift
@@ -8,13 +8,80 @@
 
 import UIKit
 
-class PushBySVViewController: UIViewController {
+class PushBySVViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet var textFields: [UITextField]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        addTapGesture()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        registerForKeyboardNotifications()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        resignKeyboardNotifications()
+    }
+}
 
-
+extension PushBySVViewController {
+    
+    private func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: nil) { [weak self] (aNoti) in
+            
+            guard let self = self else { return }
+            self.keyboardWasShown(aNoti)
+        }
+        NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: nil) { [weak self] (aNoti) in
+            
+            guard let self = self else { return }
+            self.keyboardWillBeHidden(aNoti)
+        }
+    }
+    
+    private func resignKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    private func keyboardWasShown(_ aNotification: Notification?) {
+        let info = aNotification?.userInfo
+        guard let kbSize = (info?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.size else { return }
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height + 10, right: 0.0)
+        scrollView.contentInset = contentInsets
+    }
+    
+    private func keyboardWillBeHidden(_ aNotification: Notification?) {
+        let contentInsets: UIEdgeInsets = .zero
+        scrollView.contentInset = contentInsets
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard let index = textFields.firstIndex(of: textField) else { return true }
+        let nextIndex = index + 1
+        if textFields.indices.contains(nextIndex) {
+            textFields[nextIndex].becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+        return true
+    }
+    
+    private func addTapGesture() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        scrollView.addGestureRecognizer(tap)
+    }
+    
+    @objc private func hideKeyboard() {
+        view.endEditing(true)
+    }
 }
 
